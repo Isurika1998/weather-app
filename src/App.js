2e5ed7b988b11611e6fd1a10476e2c3e0cb2d8ad
Isurika1998 <2018is055@stu.ucsc.cmb.ac.lk>
2021-04-15 23:21:49 +0530

    search weather

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import './App.css';
 import TopNav from './components/TopNav';
 import SearchBar from './components/SearchBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-const api = {
-  key: "e3d4b87722e311f852f0be32a82f0d90",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+
 
 function App() {
-  let date = String(new window.Date());
-  date = date.slice(3,15);
+
+  
+
+  
 
   return (
     <div className="app">
@@ -21,12 +18,7 @@ function App() {
       <Container maxWidth="sm">
         <div className="middle" style={{ backgroundColor: '#000000', height: '100vh', opacity: 0.5 }} >
           <SearchBar />
-          <Typography className="location" variant="h4">Bellapitiya, Sri Lanka</Typography>
-          <Typography className="date" variant="h5">{date}</Typography>
-          <div className="weather-box">
-            <Typography className="temp">37C</Typography>
-            <Typography className="weather">Sunny</Typography>
-          </div>
+          
         </div> 
       </Container>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import React, { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   search: {
@@ -21,16 +21,54 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
+const api = {
+  key: "7b48f3477845e9fd382ede953eff6ade",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 export default function BasicTextFields() {
   const classes = useStyles();
 
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  let date = String(new window.Date());
+  date = date.slice(3,15);
+
   return (
     <div>
-    <form className={classes.search} noValidate autoComplete="off">
-      <TextField className={classes.searchField} 
-      InputLabelProps={{style: { color: '#000', fontSize: '20px'  }}} 
-      fullWidth id="filled-basic" label="Search..." variant="filled" />
-    </form>
+      <div className={classes.search}>
+      <TextField
+        className={classes.searchField}
+        InputLabelProps={{style: { color: '#000', fontSize: '20px', paddingLeft: '20px'  }}} 
+        fullWidth id="filled-basic" label="Search..."
+        onChange={e => setQuery(e.target.value)}
+        value={query}
+        onKeyPress={search}
+      />
+    </div>
+    {(typeof weather.main != "undefined") ? (
+      <div>
+        <Typography className="location" variant="h4">{weather.name}, {weather.sys.country}</Typography>
+        <Typography className="date" variant="h5">{date}</Typography>
+        <div className="weather-box">
+          <Typography className="temp">{Math.round(weather.main.temp)} °C</Typography>
+          <Typography className="weather">Sunny</Typography>
+        </div>
+      </div>
+    ) : ('')}
   </div>
   );
 }
