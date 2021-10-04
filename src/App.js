00fec83ffa3c78210fb8186c6f28f842afe429b6
ManulMax <manulmadara9725@gmail.com>
2021-10-04 19:33:24 +0530

    Add report Generation

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import './App.css';
 import TopNav from './components/TopNav';
 import SearchBar from './components/SearchBar';
 import Container from '@material-ui/core/Container';
+import Pdf from "react-to-pdf";
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+buttonClass:{
+  width :'100%',
+  height : '5%',
+}
+});
 
 
 function App() {
+  const ref = React.createRef();
+  const classes = useStyles();
 
   return (
     <div className="cold">
       <TopNav />
-      <Container maxWidth="sm">
+      <Container maxWidth="sm" ref={ref}>
         <div className="middle" style={{ backgroundColor: '#000000', height: '100vh', opacity: 0.5 }} >
           <SearchBar />
-          
+          <Pdf targetRef={ref} filename="Weather_report.pdf">
+        {({ toPdf }) => <button className={classes.buttonClass}  onClick={toPdf}>Download Report</button>}
+      </Pdf> 
         </div> 
+
       </Container>
     </div>
   );
